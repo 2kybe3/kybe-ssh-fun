5d51a5b39a930d3049ed1b19f2b5d00597d75e93
2kybe3 <kybe@kybe.xyz>
2026-03-01 20:33:06 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM rust:latest AS builder
+WORKDIR /usr/src/kybe-ssh-fun
+
+RUN rustup target add x86_64-unknown-linux-musl
+
+COPY Cargo.toml Cargo.lock ./
+COPY static ./static
+COPY src ./src
+
+RUN cargo build --release --target x86_64-unknown-linux-musl
+
+FROM alpine:latest
+RUN apk add --no-cache openssh
+
+RUN adduser -D -s /usr/local/bin/kybe-ssh-fun utilsuser
+RUN echo "utilsuser:" | chpasswd
+
+RUN echo "PermitEmptyPasswords yes" >> /etc/ssh/sshd_config
+RUN echo "Port 2222" >> /etc/ssh/sshd_config
+RUN echo "HostKey /etc/ssh/ssh_host_ed25519_key" >> /etc/ssh/sshd_config
+
+RUN > /etc/motd
+
+COPY --from=builder /usr/src/kybe-ssh-fun/target/x86_64-unknown-linux-musl/release/kybe-ssh-fun /usr/local/bin/kybe-ssh-fun
+RUN chmod +x /usr/local/bin/kybe-ssh-fun
+
+EXPOSE 2222
+CMD ["/usr/sbin/sshd", "-D"]
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::env;
+
+use rustyline::{DefaultEditor, error::ReadlineError};
+
+const PGP_KEY: &str = include_str!("../static/pgp.txt");
+
+fn main() -> anyhow::Result<()> {
+    let mut rl = DefaultEditor::new()?;
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(args) = args.get(2) {
+        let args: Vec<String> = args.split(" ").map(|s| s.to_owned()).collect();
+
+        if let Some(arg) = args.first() {
+            match arg.as_str() {
+                "gpg" | "pgp" =>  {
+                    println!("{}", PGP_KEY);
+                    return Ok(());
+                }
+                _ => {
+                    println!("invalid arg supplied");
+                    return Ok(());
+                }
+            }
+        }
+    }
+
+    println!("Hello Stranger!");
+
+    loop {
+        let input = rl.readline("kybe>> ");
+        match input {
+            Ok(line) => match line.trim() {
+                "exit" | "q" | "quit" => {
+                    println!("bye");
+                    break;
+                }
+                "hello" => println!("Hello rando"),
+                "help" => println!("Avaible commands: help, hello, exit"),
+                "" => continue,
+                _ => println!("Unknown command: {}", line),
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("bye");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
